c634d5c0343cfce7ef08cfaf305e158fe1531654
Rodrigo Cornejo <rdrgcornejo@hotmail.com>
2020-05-13 23:21:02 -0300

    filtro img

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Drawing;
 using System.IO;
+using System.Reflection;
 
 namespace Backpropagation
 {
@@ -25,7 +26,9 @@ namespace Backpropagation
     public partial class MainWindow : Window
     {
         private Bitmap bitmapImagem;
-        public readonly int EPOCAS_TREINAMENTO = 10000;
+        public readonly int EPOCAS_TREINAMENTO = 100;
+
+        internal static double[][][] LUA;
 
         internal static double[][][] DADOS = new double[][][]
         {
@@ -59,6 +62,7 @@ namespace Backpropagation
         public MainWindow()
         {
             InitializeComponent();
+           
         }
 
         private void btnImagem_Click(object sender, RoutedEventArgs e) {
@@ -73,14 +77,16 @@ namespace Backpropagation
 
         private void btnRodar_Click(object sender, RoutedEventArgs e) {
             StreamWriter x;
-            string CaminhoNome = "D:\\Rodrigo\\Desktop\\VERIFICAR\\execucao.txt";
-            x = File.CreateText(CaminhoNome);
+            string caminhoExe = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string caminhoArquivo = System.IO.Path.Combine(caminhoExe, "execucao.txt");
+            x = File.CreateText(caminhoArquivo);
             BackpropagationClass backpropagation = new BackpropagationClass();
             double[] result = new double[] { 0, 0, 0, 0 };
             for (int i = 0; i < DADOS.Length; i++)
             {
                 result[i] = backpropagation.busca(DADOS[i][0]).getNeuronios()[BackpropagationClass.NEURONIO_ENTRADA + BackpropagationClass.NEURONIO_OCULTO].getSaida();
                 x.WriteLine(result[i]); ;
+                
             }
             x.WriteLine("fIM");
             x.Close();
@@ -88,17 +94,30 @@ namespace Backpropagation
 
         private void btnTreinar_Click(object sender, RoutedEventArgs e) {
             StreamWriter x;
-            string CaminhoNome = "D:\\Rodrigo\\Desktop\\VERIFICAR\\treinamento.txt";
-            x = File.CreateText(CaminhoNome);
+            string caminhoExe = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string caminhoArquivo = System.IO.Path.Combine(caminhoExe, "treino.txt");
+            x = File.CreateText(caminhoArquivo);
+            String atu;
             BackpropagationClass backpropagation = new BackpropagationClass();
             for (int j = 1; j < EPOCAS_TREINAMENTO; j++)
             {
+
+                atu = imgResultado.Text;
+                imgResultado.Text = atu + "epoca: "+j + "\n"; 
+
+
                 x.WriteLine("epoca: " + j);
                 
                 for (int i = 0; i < DADOS.Length; i++)
                 {
                     x.WriteLine(backpropagation.busca(DADOS[i][0]).recalculaErro(DADOS[i][1][0]));
+                    atu = imgResultado.Text;
+                    imgResultado.Text = atu + backpropagation.busca(DADOS[i][0]).recalculaErro(DADOS[i][1][0]) + "\n";
                 }
+
+                atu = imgResultado.Text;
+                imgResultado.Text = atu + "\n";
+
             }
 
             
@@ -116,25 +135,50 @@ namespace Backpropagation
             int height = imagem.Height;
             int[] freq = new int[256];
 
+            int cor1 = 0;
+            int cor2 = 0;
+
             for (int i = 0; i < height; i++)
             {
                 for (int j = 0; j < width; j++)
                 {
                     System.Drawing.Color cor = imagem.GetPixel(j, i);
                     freq[cor.R]++;
+                    if (cor.R==0)
+                    {
+                        cor1++;
+                    }
+                    else
+                    {
+                        cor2++;
+                    }
                 }
             }
 
+            DADOS = new double[][][]
+        {
+            new double[][]
+            {
+                new double[] {cor1, cor2},
+                 new double[] {0}
+            } };
+
+
 
             StreamWriter x;
-            string CaminhoNome = "D:\\Rodrigo\\Desktop\\VERIFICAR\\cores.txt";
-            x = File.CreateText(CaminhoNome);
+            string caminhoExe = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string caminhoArquivo = System.IO.Path.Combine(caminhoExe, "contagem.txt");
+            x = File.CreateText(caminhoArquivo);
             for (int i = 0; i < freq.Length; i++)
             {
-                x.WriteLine(i + ": " + freq[i]); ;
+                x.WriteLine(i + ": " + freq[i]);
+                
+
             }
             x.WriteLine("fIM");
             x.Close();
+
+
 
         }
     }
